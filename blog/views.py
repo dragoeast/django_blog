d6b8497f64ece5367deefb90e2ab9bc25f1bff15
Krisztian Markella <chris.krisztian.markella@gmail.com>
2023-07-24 19:42:30 -0500

    looping through the dummy posts in the home.html template

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Chris Markella',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': 'July 23, 2023',
+    },
+    {
+        'author': 'Mark Markella',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date_posted': 'July 24, 2023',
+    },
+    {
+        'author': 'Krisztian Markella',
+        'title': 'Blog Post 3',
+        'content': 'Third Post Content',
+        'date_posted': 'July 25, 2023',
+    },
+    
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts,
+    }
+    return render(request=request, template_name='blog/home.html', context=context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request=request, template_name='blog/about.html')
--- django_blog/urls.py
+++ django_blog/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(route='blog', view=include('blog.urls')),
+    path(route='', view=include('blog.urls')),
 ]
